da0452487ce2ef677d75159324445b0b0fa31ff9
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2025-02-06 12:27:17 -0300

    feat: first and second task definitions.

---
+++ file-line-parser.c
@@ -0,0 +1,6 @@
+// Task 2 -> Convert hex values present in string to integer.
+// String format example: "0000: 0x1803"
+
+// References:
+// https://stackoverflow.com/questions/10156409/convert-hex-string-char-to-int
+// https://pubs.opengroup.org/onlinepubs/7908799/xsh/strtol.html
---
+++ file-reader.c
@@ -0,0 +1,4 @@
+// Task 1 -> Read the content of a file and store in a string array.
+// File to read: "text.txt"
+
+// References: https://www.youtube.com/watch?v=DqY_PGlMihM
