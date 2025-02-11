86d6b27ca55fb4140b3b064445b50fa2b9cd67dd
Emilly Paiva <emillypaiva3260@gmail.com>
2025-02-11 20:29:59 -0300

    :sparkles: feat: cpucontext

--- src/core/cpu-context.c
+++ src/core/cpu-context.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cpu-context.h"
+#include <string.h>
 
 CPUContext *initCPU()
 {
     CPUContext *cpuCtx = (CPUContext *)calloc(1, sizeof(CPUContext));
+
+    memset(cpuCtx->registers, 0, sizeof(cpuCtx->registers));
+    cpuCtx->flags = 0;
 
     return cpuCtx;
 }
--- src/core/cpu-context.h
+++ src/core/cpu-context.h
@@ -4,6 +4,8 @@
 
 // 16 bits memory (From 0x0000 to 0xFFFF).
 #define MEMORY_RANGE 65536
+#define NUM_REGISTERS 8
+#define REGISTER_SIZE 16
 
 typedef struct cpu_context
 {
@@ -11,6 +13,11 @@ typedef struct cpu_context
     uint8_t programMem[MEMORY_RANGE];
     // 65536x8 data memory.
     uint8_t dataMem[MEMORY_RANGE];
+
+    uint8_t registers[NUM_REGISTERS][REGISTER_SIZE];
+    // Flags (1 inteiro, 32 bits, onde cada bit pode representar um flag).
+    uint32_t flags;
+
 } CPUContext;
 
 CPUContext *initCPU();
