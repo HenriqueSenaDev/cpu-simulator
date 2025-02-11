a62e2c3f7ab31e27e65ea5899638c3a8edeafae8
Emilly-Pinheiro <emillywpinheiro@gmail.com>
2025-02-11 16:48:15 -0300

    Adicionada função de popular a memória

--- src/load/alocar-mem.h
+++
@@ -1,6 +0,0 @@
-#ifndef ALOCAR_MEM_H
-#define ALOCAR_MEM_H
-
-void alocar_mem(int *memoria, char **endereco, char **valor, int nlinhas);
-
-#endif
--- src/load/file-line-parser.c
+++ src/load/file-line-parser.c
@@ -11,30 +11,17 @@ int hexadecimal_para_inteiro(const char *hex_str)
 }
 
 void popular_memoria_programa(CPUContext *cpuCtx, char **linhas, int nLinhas) {
-    // Para cada linha em "linhas" deve ser extraído o "endereço" e o "valor".
-    // É importante lembrar que ambos devem ser do tipo "uint8_t" de 8 bits.
-    // Após extraí-los, setar eles na memória de programa da cpu.
-    // Ex.: "cpuCtx->programMem[endereco8Bits] = valor8Bits;".
+    for (int i = 0; i < nLinhas; i++) {
+        char enderecostr[5];
+        char valorstr[5];
 
-    // OBS: ao finalizar pode deletar alocar-mem.h e a função comentada abaixo
-    // pois não estaremos utilizando.
-};
+        strncpy(enderecostr, linhas[i], 4);
+        enderecostr[4] = '\0';
+        strncpy(valorstr, linhas[i] + 8, 4);
+        valorstr[4] = '\0';
 
-// A função abaixo será substituída pela função acima.
-
-// void divisao_enderecos_instrucoes(char **linhas, int nlinhas, char ***endereco, char ***instrucao)
-// {
-//     *endereco = (char **)malloc(nlinhas * sizeof(char *));
-//     *instrucao = (char **)malloc(nlinhas * sizeof(char *));
-
-//     for (int i = 0; i < nlinhas; i++)
-//     {
-//         (*endereco)[i] = (char *)malloc(5 * sizeof(char));
-//         (*instrucao)[i] = (char *)malloc(5 * sizeof(char));
-
-//         strncpy((*endereco)[i], linhas[i], 4);
-//         (*endereco)[i][4] = '\0';
-//         strncpy((*instrucao)[i], linhas[i] + 8, 4);
-//         (*instrucao)[i][4] = '\0';
-//     }
-// }
+        uint8_t endereco = (uint8_t) hexadecimal_para_inteiro(enderecostr);
+        uint8_t valor = (uint8_t) hexadecimal_para_inteiro(valorstr);
+        cpuCtx->programMem[endereco] = valor;
+    }
+}
