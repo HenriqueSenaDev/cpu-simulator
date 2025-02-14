4795607c194862108102cf2c2651f9113d872b07
Emilly Pinheiro <emillywpinheiro@gmail.com>
2025-02-13 22:17:36 -0300

    Correção da função popular_memoria_programa

--- src/load/file-line-parser.c
+++ src/load/file-line-parser.c
@@ -13,19 +13,26 @@ int hexadecimal_para_inteiro(const char *hex_str)
 void popular_memoria_programa(CPUContext *cpuCtx, char **linhas, int nLinhas) {
     for (int i = 0; i < nLinhas; i++) {
         char enderecostr[5];
-        char valorstr[5];
+        char valorstr1[3];
+        char valorstr2[3];
 
         strncpy(enderecostr, linhas[i], 4);
         enderecostr[4] = '\0';
-        strncpy(valorstr, linhas[i] + 8, 4);
-        valorstr[4] = '\0';
+        strncpy(valorstr1, linhas[i] + 10, 2);
+        valorstr1[2] = '\0';
+        strncpy(valorstr2, linhas[i] + 8, 2);
+        valorstr2[2] = '\0';
+
 
         uint8_t endereco = (uint8_t) hexadecimal_para_inteiro(enderecostr);
-        uint8_t valor = (uint8_t) hexadecimal_para_inteiro(valorstr);
-        cpuCtx->programMem[endereco] = valor;
+        uint8_t valor1 = (uint8_t) hexadecimal_para_inteiro(valorstr1);
+        uint8_t valor2 = (uint8_t) hexadecimal_para_inteiro(valorstr2);
+        cpuCtx->programMem[endereco +1 ] = valor1;
+        cpuCtx->programMem[endereco] = valor2;
     }
+    
     for(int i = 0; i < MEMORY_RANGE; i++) {
         if(cpuCtx->programMem[i] != 0)
-        printf("Memória[%d] = %d\n", i, cpuCtx->programMem[i]);
+        printf("Memória[%d] = %02x\n", i, cpuCtx->programMem[i]);
     }
 }
