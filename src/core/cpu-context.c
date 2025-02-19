4fa6a2e65d2edaa1d6ac04138fae9ff181ac274a
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2025-02-18 22:38:39 -0300

    feat: search instruction and convert to binary.

--- src/core/cpu-context.c
+++ src/core/cpu-context.c
@@ -24,11 +24,6 @@ CPUContext *initCPU()
     return cpuCtxPtr;
 }
 
-void cleanCPU(CPUContext *cpuCtxPtr)
-{
-    free(cpuCtxPtr);
-}
-
 void printProgramMem(CPUContext *cpuCtxPtr)
 {
     printf("----------- Program Memory -----------\n");
@@ -38,7 +33,7 @@ void printProgramMem(CPUContext *cpuCtxPtr)
         if (cpuCtxPtr->usedProgramMem[i])
         {
             printf(
-                "Addr[0x%04x] = 0x%02x = %d\n",
+                "Addr[0x%04x]: 0x%02x = %d\n",
                 i,
                 cpuCtxPtr->programMem[i],
                 cpuCtxPtr->programMem[i]);
@@ -49,14 +44,56 @@ void printProgramMem(CPUContext *cpuCtxPtr)
 void startExecution(CPUContext *cpuCtxPtr)
 {
     printf("------- Start Execution -------\n");
+
+    while (cpuCtxPtr->usedProgramMem[cpuCtxPtr->pc])
+    {
+        uint8_t *instruction = (uint8_t *)calloc(16, sizeof(uint8_t));
+
+        if (instruction == NULL)
+        {
+            perror("Error on alocate memory for instruction.");
+            exit(1);
+        }
+
+        uint8_t instructionLsb = cpuCtxPtr->programMem[cpuCtxPtr->pc];
+        uint8_t instructionMsb = cpuCtxPtr->programMem[cpuCtxPtr->pc + 1];
+
+        for (int i = 0; i < 8; i++)
+        {
+            uint8_t lsbBit = instructionLsb % 2; // 0 or 1
+            instruction[15 - i] = lsbBit;
+
+            uint8_t msbBit = instructionMsb % 2; // 0 or 1
+            instruction[7 - i] = msbBit;
+
+            instructionLsb = instructionLsb >> 1;
+            instructionMsb = instructionMsb >> 1;
+        }
+
+        printf("Instruction of address %d = ", cpuCtxPtr->pc);
+        for (int i = 0; i < 16; i++)
+        {
+            printf("%d", instruction[i]);
+            if (i == 7)
+                printf(" ");
+        }
+
+        printf("\n");
+
+        // Todo: place value on IR.
+        cpuCtxPtr->pc += 2;
+    }
+
     endExecution(cpuCtxPtr);
 }
 
 void endExecution(CPUContext *cpuCtxPtr)
 {
     printf("-------- End Execution --------\n");
-    cleanCPU(cpuCtxPtr);
+
+    free(cpuCtxPtr);
     printf("CPU memory freed.\n");
+
     printf("TODO: print cpu state.\n");
     exit(0);
 }
