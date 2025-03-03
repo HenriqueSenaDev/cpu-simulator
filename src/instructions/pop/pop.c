841260b450d66e67ee62eabc67eebde16e000642
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2025-03-03 16:15:10 -0300

    feat: PSH and POP instructions implementation.

--- src/instructions/pop/pop.c
+++ src/instructions/pop/pop.c
@@ -1,10 +1,42 @@
 #include "../../core/cpu-context.h"
+#include "../../utils/numeric.h"
 #include <stdint.h>
 #include <stdio.h>
 
 void POP(CPUContext *cpuCtxPtr, uint8_t rd)
 {
-  printf("SP++; R%d = [SP]\n", rd);
+  printf("POP R%d\n", rd);
 
-  // TODO
+  cpuCtxPtr->sp += 2;
+
+  uint8_t msByte = cpuCtxPtr->dataMem[cpuCtxPtr->sp + 1];
+  uint8_t lsByte = cpuCtxPtr->dataMem[cpuCtxPtr->sp];
+
+  cpuCtxPtr->dataMem[cpuCtxPtr->sp + 1] = 0;
+  cpuCtxPtr->dataMem[cpuCtxPtr->sp] = 0;
+
+  uint8_t *msByteBits = decimalToBinary(msByte);
+  uint8_t *lsByteBits = decimalToBinary(lsByte);
+
+  uint8_t totalBits[16] = {
+      msByteBits[0],
+      msByteBits[1],
+      msByteBits[2],
+      msByteBits[3],
+      msByteBits[4],
+      msByteBits[5],
+      msByteBits[6],
+      msByteBits[7],
+      msByteBits[0],
+      lsByteBits[1],
+      lsByteBits[2],
+      lsByteBits[3],
+      lsByteBits[4],
+      lsByteBits[5],
+      lsByteBits[6],
+      lsByteBits[7],
+  };
+
+  uint16_t *value = binaryToDecimal(totalBits, 0, 15);
+  cpuCtxPtr->registers[rd] = *value;
 }
--- src/instructions/psh/psh.c
+++ src/instructions/psh/psh.c
@@ -1,10 +1,20 @@
 #include "../../core/cpu-context.h"
+#include "../../utils/numeric.h"
 #include <stdint.h>
 #include <stdio.h>
 
 void PSH(CPUContext *cpuCtxPtr, uint8_t rn)
 {
-  printf("PSH [SP] = R%d; SP--\n", rn);
+  printf("PSH R%d\n", rn);
 
-  // TODO
+  uint16_t value = cpuCtxPtr->registers[rn];
+
+  uint8_t *bitsPtr = decimalToBinary(value);
+  uint8_t msByte = (uint8_t)*(binaryToDecimal(bitsPtr, 0, 7));
+  uint8_t lsByte = (uint8_t)*(binaryToDecimal(bitsPtr, 8, 15));
+
+  cpuCtxPtr->dataMem[cpuCtxPtr->sp + 1] = msByte;
+  cpuCtxPtr->dataMem[cpuCtxPtr->sp] = lsByte;
+
+  cpuCtxPtr->sp -= 2;
 }
