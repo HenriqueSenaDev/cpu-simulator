7c106f5def2bcac8a74378e4dc11077c0bb633f1
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2025-03-03 15:51:28 -0300

    feat: STR instruction implementation.

--- src/instructions/str/str.c
+++ src/instructions/str/str.c
@@ -1,4 +1,5 @@
 #include "../../core/cpu-context.h"
+#include "../../utils/numeric.h"
 #include <stdint.h>
 #include <stdio.h>
 
@@ -6,12 +7,26 @@ void STR(CPUContext *cpuCtxPtr, uint8_t rm, uint8_t rn)
 {
   printf("STR [R%d] = R%d\n", rm, rn);
 
-  // TODO
+  uint16_t addr = cpuCtxPtr->registers[rm];
+  uint16_t value = cpuCtxPtr->registers[rn];
+
+  uint8_t *bitsPtr = decimalToBinary(value);
+
+  uint8_t msByte = (uint8_t)*(binaryToDecimal(bitsPtr, 0, 7));
+  uint8_t lsByte = (uint8_t)*(binaryToDecimal(bitsPtr, 8, 15));
+
+  cpuCtxPtr->dataMem[addr + 1] = msByte;
+  cpuCtxPtr->dataMem[addr] = lsByte;
+  cpuCtxPtr->usedDataMem[addr + 1] = true;
+  cpuCtxPtr->usedDataMem[addr] = true;
 }
 
 void STR_IM(CPUContext *cpuCtxPtr, uint8_t rm, uint8_t immediate)
 {
   printf("STR [R%d] = #%d\n", rm, immediate);
 
-  // TODO
+  uint16_t addr = cpuCtxPtr->registers[rm];
+
+  cpuCtxPtr->dataMem[addr] = immediate;
+  cpuCtxPtr->usedDataMem[addr] = true;
 }
