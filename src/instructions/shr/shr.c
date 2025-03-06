bba52f6b360225a2093166b58009fa01752e779f
Emilly-Pinheiro <emillywpinheiro@gmail.com>
2025-03-06 16:44:46 -0300

    correção flag de signal em SHR

--- src/instructions/shr/shr.c
+++ src/instructions/shr/shr.c
@@ -9,7 +9,6 @@ void SHR(CPUContext *cpuCtxPtr, uint8_t rd, uint8_t rm, uint8_t immediate)
   cpuCtxPtr->registers[rd] = cpuCtxPtr->registers[rm] >> immediate;
 
   cpuCtxPtr->zero = cpuCtxPtr->registers[rd] == 0;
-  cpuCtxPtr->signal = (cpuCtxPtr->registers[rd] & 0x8000) != 0;
   // o bit de carry é o bit que foi deslocado para fora do registrador, nesse caso o bit MENOS significativo.
   // se immediate = 0 não houve deslocamento, então o carry é 0
   // (immediate > 0 && immediate <= 16) ? serve para verificar se o immediate é válido 
